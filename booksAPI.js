e4675a6775c7a6576c3363eabce1d859303172a7
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-08-01 22:02:19 +0545

    update...

---
+++ booksAPI.js
@@ -0,0 +1,9 @@
+const booksAPI = "https://openlibrary.org/books/OL7353617M.json";
+
+// console.log(booksAPI);
+
+fetch(booksAPI)
+.then( response =>  response;
+.then((data)=>{
+    console.log(data)
+})
